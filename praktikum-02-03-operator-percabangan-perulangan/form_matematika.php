4ab635c1f1ceef24cd52ec663f0b0faec2376081
Alanjayapranata <alanjayapranata@gmail.com>
2022-11-14 11:40:42 +0800

    tugas baru

---
+++ praktikum-02-03-operator-percabangan-perulangan/form_matematika.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>From matematika</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <h1 class="text-center">Operation Aritmatika</h1>
+</head>
+<body>
+    <p class="text-center">Nim_20202205056 - Nama_Alanjayapranata</p>
+
+    </div>
+</body>
+</html>
+
+
+<?php
+
+$t = date("H");
+
+if ($t < "20") {
+  echo "Have a good day!";
+}
+
+
+?>
